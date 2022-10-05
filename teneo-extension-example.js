6d4fe7c27f01babaa246e40686d1ee4a5b7eb3da
jpicaswe <115085551+jpicaswe@users.noreply.github.com>
2022-10-05 15:27:15 +0200

    Update teneo-extension-example.js
    updated id typing

--- teneo-extension-example.js
+++ teneo-extension-example.js
@@ -1,5 +1,4 @@
 // this was update 3:20
 
-var data = Document.getElementById("scriptdata");
-
+var data = document.getElementById("scriptdata");
 data.innerHTML = "this was updated through script file at 3:22";
